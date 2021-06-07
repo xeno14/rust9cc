6df94db02ceb71ea38781a372a2150e1111d096d
xeno14 <integral14.dev@gmail.com>
2021-06-07 21:31:03 +0800

    --mode argument using clap

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,45 @@
-use std::env;
-
+use clap::{App, Arg};
 use rust9cc::*;
 
+const MODE_AST: &str = "ast";
+const MODE_TOKEN: &str = "token";
+const MODE_X86: &str = "x86";
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        panic!("Wrong number of arguments.");
+    let matches = App::new("rust9cc")
+        .version("0.0.1")
+        .arg(
+            Arg::with_name("mode")
+                .long("mode")
+                .possible_values(&[MODE_AST, MODE_TOKEN, MODE_X86])
+                .default_value(MODE_X86)
+        )
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Input expression.")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let input = matches.value_of("INPUT").unwrap();
+    let tokens = tokenize(input).unwrap();
+    
+    let mode = matches.value_of("mode").unwrap();
+    if mode == MODE_TOKEN {
+        for token in tokens.iter() {
+            println!("{:?}", token);
+        }
+        return;
     }
 
-    let input = args.get(1).unwrap();
-    let tokens = tokenize(input).unwrap();
-    // eprintln!("{:?}", tokens);
-    // compile(&mut tokens.into_iter().peekable()).unwrap();
     let tokens = &mut tokens.into_iter().peekable();
     let root = parse_into_ast(tokens).unwrap();
-    // eprintln!("{:?}", root);
+
+    if mode == MODE_AST {
+        println!("{:?}", root);
+        return;
+    }
 
     gen(&root).unwrap();
 }
